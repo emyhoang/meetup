b9b6f8c5f14290c48721c8d7bb2c152ef2dc89e0
emyhoang <emyhoang8899@gmail.com>
2019-01-06 13:41:22 -0800

    adding a custom error message page when a route is not found

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const createError = require('http-errors');
 const path = require('path');
 
 app.set('view engine', 'pug');
@@ -16,6 +17,19 @@ app.get('/favicon.ico', (req, res, next) => {
 });
 
 app.use('/', routes());
+
+app.use((req, res, next) => {
+  return next(createError(404, 'File not found'));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  const status = err.status || 500;
+  res.locals.status = status;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(status);
+  return res.render('error');
+});
 
 app.listen(3000);
 
