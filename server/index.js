917ce34facfa9f6c8e87d51a58935132725b071e
emyhoang <emyhoang8899@gmail.com>
2019-01-06 13:26:55 -0800

    add pug to the project

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './views'));
+
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
 
 const routes = require('./routes');
 app.use(express.static('public'));
